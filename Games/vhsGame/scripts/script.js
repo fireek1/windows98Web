eef959ecabadd98417d156194b415656aa86f919
fireek1 <odvdplvdplv@gmail.com>
2023-07-05 17:38:01 +0200

    0.0.2

---
+++ Games/vhsGame/scripts/script.js
@@ -0,0 +1,181 @@
+const cube = document.querySelector('.cube')
+const scene = document.querySelector('.scene')
+const ui = document.querySelector('.ui')
+const pyramid = document.querySelector('.pyramid')
+const enemy = document.querySelector('.eye')
+const grass = document.querySelector('.grass')
+
+let translateZ = 0
+let translateX = 0
+
+let rotateX = 0
+let rotateY = 0
+let rotateZ = 0
+
+let prevMouseX = 0
+let prevMouseY = 0
+
+let originZ = 0
+let originX = 0
+
+let perspective = 500
+let cover = 0
+let blur = 0
+
+let dx = 0
+let dy = 0
+
+let isMoving = false
+let currentKey = null
+
+const speed = 7
+
+let enemyCount = 0
+
+let mouseCanMove = true
+
+document.onkeyup = (e) => {
+    if (e.code === 'KeyW' || e.code === 'KeyA' || e.code === 'KeyS' || e.code === 'KeyD') {
+        dx = 0
+        dy = 0
+    }
+}
+
+document.onmousemove = (e) => {
+    if (mouseCanMove) {
+    const currentMouseX = e.clientX
+    const currentMouseY = e.clientY
+
+    const deltaX = (currentMouseX - prevMouseX)
+    const deltaY = currentMouseY - prevMouseY
+    const deltaZ = (currentMouseX - prevMouseX)
+
+    rotateY += -deltaY * 0.2
+    rotateX += deltaX * 0.2
+    rotateZ += -deltaY * 0.2
+
+    prevMouseX = currentMouseX
+    prevMouseY = currentMouseY
+    }
+    
+    
+}
+
+function moveChatacter() {
+    if (isMoving) {
+        cube.style.transform = `translate3d(${translateX}px, 0, ${translateZ}px) rotateY(${rotateX}deg) rotateX(${rotateY * Math.cos(rotateX * Math.PI / 180)}deg) rotateZ(${rotateZ * Math.sin(rotateX * Math.PI / 180)}deg)`
+        cube.style.transformOrigin = `${originX}px 50% ${originZ}px`
+        requestAnimationFrame(moveChatacter)
+    }
+}
+
+let coverEye = 0
+
+document.onkeydown = (e) => {
+    if (e.code === 'KeyW' || e.code === 'KeyA' || e.code === 'KeyS' || e.code === 'KeyD') {
+        isMoving = true
+        currentKey = e.code
+        moveChatacter()
+    } else if (e.code === 'Space') {
+        rotateX = 0
+        rotateY = 0
+        rotateZ = 0
+        perspective = 500
+    } else if (e.code === 'ArrowUp') {
+        perspective += 10
+    } else if (e.code === 'ArrowDown') {
+        perspective -= 10
+    } else if (e.code === 'ArrowLeft' && blur > 0) {
+        blur -= 1
+    } else if (e.code === 'ArrowRight') {
+        blur += 1
+    }
+
+    if (translateZ >= 5000 && cover < 1) {
+        cover += 0.005
+    }
+
+    scene.style.perspective = `${perspective}px`
+    ui.style.backgroundColor = `rgba(0, 0, 0, ${cover})`;
+    ui.style.backdropFilter = `blur(${blur}px) brightness(1.25)`;
+}
+
+document.onkeyup = (e) => {
+    if (e.code === 'KeyW' || e.code === 'KeyA' || e.code === 'KeyS' || e.code === 'KeyD') {
+        isMoving = false
+        currentKey = null
+    }
+}
+
+setInterval(function () {
+    if (isMoving && currentKey !== null) {
+        if (currentKey === "KeyW") {
+            dx = -Math.sin(rotateX * Math.PI / 180)
+            dy = Math.cos(rotateX * Math.PI / 180)
+            translateX += dx * speed
+            translateZ += dy * speed
+        } else if (currentKey === "KeyA") {
+            dx = Math.sin(rotateX * Math.PI / 180 + Math.PI / 2)
+            dy = -Math.cos(rotateX * Math.PI / 180 + Math.PI / 2)
+            translateX += dx * speed
+            translateZ += dy * speed
+        } else if (currentKey === "KeyS") {
+            dx = Math.sin(rotateX * Math.PI / 180)
+            dy = -Math.cos(rotateX * Math.PI / 180)
+            translateX += dx * speed
+            translateZ += dy * speed
+        } else if (currentKey === "KeyD") {
+            dx = -Math.sin(rotateX * Math.PI / 180 + Math.PI / 2)
+            dy = Math.cos(rotateX * Math.PI / 180 + Math.PI / 2)
+            translateX += dx * speed
+            translateZ += dy * speed
+        }
+        originX = -translateX + 150
+        originZ = -translateZ + 500
+    }
+}, 1000 / 60);
+
+
+
+
+
+
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function setFog() {
+
+}
+
+function spawnEnemy() {
+    const newEnemy = enemy.cloneNode(true);
+    ui.appendChild(newEnemy)
+    newEnemy.style.left = `${random(25, 75)}%`
+    newEnemy.style.top = `${random(0, 100)}%`
+    newEnemy.style.scale = `${random(0.5, 3)}`
+    newEnemy.style.opacity = `1`
+    grass.style.height = `100000px`
+}
+
+setInterval(() => {
+    if (cover >= 1) {
+        spawnEnemy()
+    }
+}, 3000)
+
+function Frame() {
+    if (cover >= 1) {
+        rotateX++
+        rotateY++
+        rotateZ++
+        mouseCanMove = false
+    }
+
+    cube.style.transform = `translate3d(${translateX}px, 0, ${translateZ}px) rotateY(${rotateX}deg) rotateX(${rotateY * Math.cos(rotateX * Math.PI / 180)}deg) rotateZ(${rotateZ * Math.sin(rotateX * Math.PI / 180)}deg)`
+
+    requestAnimationFrame(Frame)
+}
+
+Frame()
--- scripts/script.js
+++ scripts/script.js
@@ -4,27 +4,48 @@ const progs = document.querySelectorAll('.prog')
 const progsName = document.querySelectorAll('.prog > p')
 const time = document.querySelector('.time > p')
 
+let winId = 0
+let mouseMove = false
+let el
+
 for (let i = 0; i < progs.length; i++) {
     progsImg[i].onclick = () => {
-        if (progs[i].classList[0] === 'note') {
-            setWindow(progs[i].classList[0], progs[i].dataset.name, progs[i].dataset.content)
-        }
+        const type = progs[i].classList[0]
+        const name = progs[i].dataset.name
+        const content = progs[i].dataset.content
+        setWindow(type, name, content)
     }
 }
 
 
-
-progsName.onclick = () => {
-    
-}
-
 function windowBtn() {
     const close = document.querySelectorAll('.close')
+    const full = document.querySelectorAll('.full')
     for (let i = 0; i < close.length; i++) {
         close[i].onclick = () => {
-            document.querySelector('.note').dataset.content = document.querySelector('textarea').value
-            desktop.removeChild(document.querySelector('.window'))
+            const window = document.querySelector(`#window${close[i].id.slice(5)}`)
+            if (window.dataset.type === 'note') {
+                document.querySelector(`[data-name=${document.querySelector("#window" + close[i].id.slice(5) + "> .window-header > .window-title > .title-name").textContent}`).dataset.content = document.querySelector('textarea').value
+            }
+
+            desktop.removeChild(window)
         }
+    }
+
+    for (let i = 0; i < full.length; i++) {
+        full[i].onclick = () => {
+            document.querySelector(`#window${full[i].id.slice(4)}`).style.width = 100 + 'vw'
+            document.querySelector(`#window${full[i].id.slice(4)}`).style.height = 100 + 'vh'
+
+            document.querySelector(`#window${full[i].id.slice(4)}`).style.top = 0
+            document.querySelector(`#window${full[i].id.slice(4)}`).style.left = 0
+        }
+    }
+}
+
+function setName() {
+    for (let i = 0; i < progs.length; i++) {
+        progsName[i].textContent = progs[i].dataset.name
     }
 }
 
@@ -32,16 +53,19 @@ function setWindow(type, name, content) {
     if (type === 'note') {
         const win = document.createElement('div')
         win.classList.add('window')
+        winId++
+        win.id = `window${winId}`
+        win.dataset.type = type
         win.innerHTML = `
         <div class="window-header">
             <div class="window-title">
                 <img src="img/note.png" alt="">
-                <p class="title-name">${name} - NotePad</p>
+                <p class="title-name">${name}</p>
             </div>
             <div class="header-btns">
-                <img src="img/btnFold.svg" alt="" class="btn fold">
-                <img src="img/btnFull.svg" alt="" class="btn full">
-                <img src="img/btnClose.svg" alt="" class="btn close">
+                <img src="img/btnFold.svg" id="fold${winId}" alt="" class="btn fold">
+                <img src="img/btnFull.svg" id="full${winId}" alt="" class="btn full">
+                <img src="img/btnClose.svg" id="close${winId}" alt="" class="btn close">
             </div>
         </div>
 
@@ -53,18 +77,144 @@ function setWindow(type, name, content) {
         </div>
 
         <div class="content">
-            <textarea name="" cols="90" rows="25" aria-disabled="true">${content}</textarea>
+            <textarea name="" cols="90" rows="18" aria-disabled="true">${content}</textarea>
         </div>`
+        win.style.top = `${Math.random() * 500}px`
+        win.style.left = `${Math.random() * 1000}px`
+        desktop.appendChild(win)
+    } else if (type === 'vhsGame') {
+        const win = document.createElement('div')
+        win.classList.add('window')
+        winId++
+        win.id = `window${winId}`
+        win.dataset.type = type
+        win.innerHTML = `
+            <div class="window-header">
+                <div class="window-title">
+                    <img src="img/note.png" alt="">
+                    <p class="title-name">${name}</p>
+                </div>
+                <div class="header-btns">
+                    <img src="img/btnFold.svg" id="fold${winId}" alt="" class="btn fold">
+                    <img src="img/btnFull.svg" id="full${winId}" alt="" class="btn full">
+                    <img src="img/btnClose.svg" id="close${winId}" alt="" class="btn close">
+                </div>
+            </div>
+
+            <div class="edit">
+                <p class="file opt"><span>F</span>ile</p>
+                <p class="edit opt"><span>E</span>dit</p>
+                <p class="search opt"><span>S</span>earch</p>
+                <p class="help opt"><span>H</span>elp</p>
+            </div>
+
+            <div class="content">
+                <iframe src="Games/vhsGame/vhsGame.html" frameborder="0" width="100%" height="100%"></iframe>
+            </div>`
+        win.style.top = `${Math.random() * 500}px`
+        win.style.left = `${Math.random() * 1000}px`
+        desktop.appendChild(win)
+    } else if (type === 'squareGame') {
+        const win = document.createElement('div')
+        win.classList.add('window')
+        winId++
+        win.id = `window${winId}`
+        win.dataset.type = type
+        win.innerHTML = `
+            <div class="window-header">
+                <div class="window-title">
+                    <img src="img/note.png" alt="">
+                    <p class="title-name">${name}</p>
+                </div>
+                <div class="header-btns">
+                    <img src="img/btnFold.svg" id="fold${winId}" alt="" class="btn fold">
+                    <img src="img/btnFull.svg" id="full${winId}" alt="" class="btn full">
+                    <img src="img/btnClose.svg" id="close${winId}" alt="" class="btn close">
+                </div>
+            </div>
+
+            <div class="edit">
+                <p class="file opt"><span>F</span>ile</p>
+                <p class="edit opt"><span>E</span>dit</p>
+                <p class="search opt"><span>S</span>earch</p>
+                <p class="help opt"><span>H</span>elp</p>
+            </div>
+
+            <div class="content">
+                <iframe src="Games/squareGame.html" frameborder="0" width="100%" height="100%"></iframe>
+            </div>`
+        win.style.top = `${Math.random() * 500}px`
+        win.style.left = `${Math.random() * 1000}px`
+        win.style.height = `900px`
+        win.style.width = `1200px`
+        desktop.appendChild(win)
+    } else if (type === 'internet') {
+        const win = document.createElement('div')
+        win.classList.add('window')
+        winId++
+        win.id = `window${winId}`
+        win.dataset.type = type
+        win.innerHTML = `
+            <div class="window-header">
+                <div class="window-title">
+                    <img src="img/note.png" alt="">
+                    <p class="title-name">${name}</p>
+                </div>
+                <div class="header-btns">
+                    <img src="img/btnFold.svg" id="fold${winId}" alt="" class="btn fold">
+                    <img src="img/btnFull.svg" id="full${winId}" alt="" class="btn full">
+                    <img src="img/btnClose.svg" id="close${winId}" alt="" class="btn close">
+                </div>
+            </div>
+
+            <div class="edit">
+                <p class="file opt"><span>F</span>ile</p>
+                <p class="edit opt"><span>E</span>dit</p>
+                <p class="search opt"><span>S</span>earch</p>
+                <p class="help opt"><span>H</span>elp</p>
+            </div>
+
+            <div class="content">
+                <iframe src="https://oldgoogle.neocities.org/1998/" frameborder="0" width="100%" height="100%"></iframe>
+            </div>`
         win.style.top = `${Math.random() * 500}px`
         win.style.left = `${Math.random() * 1000}px`
         desktop.appendChild(win)
     }
 }
 
+function normalize() {
+
+}
 
 
-function moveWindow() {
+function mouseDown() {
+    const windows = document.querySelectorAll('.window')
+    const headers = document.querySelectorAll('.window-header')
+    if (windows.length !== 0) {
+        for (let i = 0; i < windows.length; i++) {
+            const window = windows[i]
+            const header = headers[i]
+            header.onmousedown = (e) => {
+                mouseMove = true
+                el = document.querySelector(`#${window.id}`)
+            }
+            header.onmouseup = (e) => {
+                mouseMove = false
+                el = null
+            }
+        }
+    }
 
+}
+
+function moveWindow(el) {
+    window.onmousemove = (e) => {
+        if (mouseMove && el) {
+            el.style.top = e.screenY - 100 + 'px'
+            el.style.left = e.screenX - 100 + 'px'
+        }
+    }
 }
 
 function timeSet() {
@@ -83,6 +233,9 @@ function timeSet() {
 function Frame() {
     timeSet()
     windowBtn()
+    mouseDown()
+    setName()
+    moveWindow(el)
 
     requestAnimationFrame(Frame)
 }
